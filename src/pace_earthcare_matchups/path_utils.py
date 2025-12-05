f427d84879287cb8b6b07e4edeaeaa692734b061
Sean Foley <sean.r.foley@nasa.gov>
2025-12-04 20:29:20 -0500

    only use envvar for data path

--- src/pace_earthcare_matchups/path_utils.py
+++ src/pace_earthcare_matchups/path_utils.py
@@ -4,8 +4,8 @@ from pathlib import Path
 from maap.Result import Granule
 from pystac.item import Item
 
-PATH_ROOT = Path(os.getenv('PACE_ROOT_PATH', (Path(__file__) / ".." / ".." / "..").resolve()))
-PATH_DATA = (PATH_ROOT / "data").resolve()
+PATH_ROOT = (Path(__file__) / ".." / ".." / "..").resolve()
+PATH_DATA = Path(os.getenv("PACE_DATA_PATH", PATH_ROOT / "data")).resolve()
 PATH_TOKEN = (PATH_ROOT / "token.txt").resolve()
 
 
